df9c5eac9967ee20a6f1f4147564693992487d27
Firat Baydemir <firatbaydemirr@gmail.com>
2025-10-27 17:28:59 +0100

    firebase-config

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AlzaSyDU...[sizin kodunuz]...rw5k-0",
+const firebaseConfig = {
+    apiKey: "AIzaSyDiJ28SOe_9lg0mtMSsbhrP7jfbbRw5k-0",
     authDomain: "kiosk-a4014.firebaseapp.com",
     projectId: "kiosk-a4014",
-    storageBucket: "kiosk-a4014.appspot.com",
-    messagingSenderId: "34265671523",
-    appId: "1:342656...[sizin kodunuz]...a0d784556",
-    measurementId: "G-YBZ...[sizin kodunuz]"
+    storageBucket: "kiosk-a4014.firebasestorage.app",
+    messagingSenderId: "342656071523",
+    appId: "1:342656071523:web:b41995a2e1a37a0d784556",
+    measurementId: "G-YBZX96NK3H"
   };
